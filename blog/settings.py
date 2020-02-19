94d8845a7d83837ece11a97cdcd252323e981886
davidhedderman <davidheddermancodes@gmail.com>
2020-02-19 23:21:31 +0000

    fixed bundle name for vue app

--- blog/settings.py
+++ blog/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-FRONTEND_DIR = os.path.join(BASE_DIR, 'apps')
+FRONTEND_DIR = os.path.join(BASE_DIR, 'vue-apps')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
